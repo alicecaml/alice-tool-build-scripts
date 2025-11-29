769b3fd7afdc97621512501f35026f3392f0dd62
Stephen Sherratt <stephen@sherra.tt>
2025-11-29 12:26:00 +1100

    Fix name of dot-merlin-reader archive on linux-gnu x86_64 again

--- 5.3.1/dot-merlin-reader.5.4.1-503/build-x86_64-linux-gnu.sh
+++ 5.3.1/dot-merlin-reader.5.4.1-503/build-x86_64-linux-gnu.sh
@@ -2,5 +2,5 @@
 set -ex
 
 docker buildx build --output type=local,dest=./out -f ubuntu-x86_64.dockerfile .
-mv out/dot-merlin-reader-5.4.1-503-built-with-ocaml.5.3.1+relocatable-x86_64-linux-gnu.tar.gz .
+mv out/dot-merlin-reader-5.4.1-503-built-with-ocaml-5.3.1+relocatable-x86_64-linux-gnu.tar.gz .
 rm -rf out
